e8d9249ceb48d103bad6674c0212380869721940
rigolinux <rigoberto7567@gmail.com>
2022-10-17 12:14:32 -0600

    Agregar archivos de proyecto.

---
+++ CRUD-mvc/Models/NET_CRUDContext.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CRUD_mvc.Models
+{
+    public partial class NET_CRUDContext : DbContext
+    {
+        public NET_CRUDContext()
+        {
+        }
+
+        public NET_CRUDContext(DbContextOptions<NET_CRUDContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+  //              optionsBuilder.UseSqlServer("server=localhost; database=NET_CRUD; integrated security=true;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.ToTable("USERS");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.ActiveUser).HasColumnName("Active_User");
+
+                entity.Property(e => e.FirstName)
+                    .HasMaxLength(30)
+                    .IsUnicode(false)
+                    .HasColumnName("First_Name");
+
+                entity.Property(e => e.Gender)
+                    .HasMaxLength(1)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Gmail)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LastName)
+                    .HasMaxLength(30)
+                    .IsUnicode(false)
+                    .HasColumnName("Last_Name");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ CRUD-mvc/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRUD_mvc.Models
+{
+    public partial class User
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Gmail { get; set; }
+        public string? Gender { get; set; }
+        public int? Age { get; set; }
+        public bool? ActiveUser { get; set; }
+    }
+}
